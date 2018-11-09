3576a3853f14b2b0ad68c069e87b344842c07884
Darren Li <beckzayi@126.com>
2018-11-09 11:07:17 +1100

    Updates

---
+++ precache-manifest.b377cd96e08e7921a2c929d5308e94f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/investment-portfolio-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c92638b398086731b4e4",
+    "url": "/investment-portfolio-app/static/js/runtime~main.c92638b3.js"
+  },
+  {
+    "revision": "4e13c197ea11d7d2a918",
+    "url": "/investment-portfolio-app/static/js/main.4e13c197.chunk.js"
+  },
+  {
+    "revision": "c13667f2b689330052fc",
+    "url": "/investment-portfolio-app/static/js/1.c13667f2.chunk.js"
+  },
+  {
+    "revision": "4e13c197ea11d7d2a918",
+    "url": "/investment-portfolio-app/static/css/main.936d485a.chunk.css"
+  },
+  {
+    "revision": "c13667f2b689330052fc",
+    "url": "/investment-portfolio-app/static/css/1.4e48af13.chunk.css"
+  },
+  {
+    "revision": "26a14111b3ebfe6dce4e9a2b89c8c3a8",
+    "url": "/investment-portfolio-app/index.html"
+  }
+];
